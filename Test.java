e5f1b92fd6669264abc91aaf66e2a4a15f1fca23
gbernsteiner <guetar@gmx.at>
2012-12-01 15:50:40 +0100

    0004 Created Track

--- Test.java
+++ Test.java
@@ -4,4 +4,9 @@
  * @author guetar
  */
 public class Test {
+    
+    public static void main(String[] args) {
+    
+        Track track = new Track(10, 20);
+    }
 }
--- Track.java
+++ Track.java
@@ -4,4 +4,10 @@
  * @author guetar
  */
 public class Track {
+    
+    private Car[][] track;
+    
+    public Track(int width, int height) {
+        track = new Car[width][height];
+    }
 }
