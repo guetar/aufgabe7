ce9fb50252dd1ea1211c07928c85403182d2c260
Matthias <Matthias@128.131.209.198>
2012-12-01 16:55:11 +0100

    end in track

--- Track.java
+++ Track.java
@@ -5,10 +5,18 @@
  */
 public class Track {
     
+    //IV: gibt an, ob ein Auto die Maximalpunktezahl (10) erreicht hat, oder 
+    //ein Auto eine Maximalzahl an Feldwechseln ausgefuehrt hat
+    private boolean end=false;
     private CarQuick[][] track;
     
     public Track(int width, int height) {
         track = new CarQuick[width][height];
+    }
+    
+    //NB: end wird auf true gesetzt und alle Threads sollen beendet werden
+    public void gameOver(){
+    end=true;
     }
     
     public void setCar(int x, int y, CarQuick c) {
